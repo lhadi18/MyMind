88279d703b3d9b3484490d95b67216579e2a0fa0
Kian Azizkhani <kazizkhani@my.bcit.ca>
2022-04-27 19:27:06 -0700

    Login/Logout/Register backend coding done

--- server.js
+++ server.js
@@ -1,25 +1,72 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').load()
-  }
-  
-  const express = require('express')
-  const app = express()
-  const expressLayouts = require('express-ejs-layouts')
-  
-  const indexRouter = require('./routes/index')
-  
-  app.set('view engine', 'ejs')
-  app.set('views', __dirname + '/views')
-  app.set('layout', 'layouts/layout')
-  app.use(expressLayouts)
-  app.use(express.static('public'))
-  
-  const mongoose = require('mongoose')
-  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-  const db = mongoose.connection
-  db.on('error', error => console.error(error))
-  db.once('open', () => console.log('Connected to Mongoose'))
-  
-  app.use('/', indexRouter)
-  
-  app.listen(process.env.PORT || 3000)
+const express               =  require('express'),
+      app                   =  express(),
+      mongoose              =  require("mongoose"),
+      passport              =  require("passport"),
+      bodyParser            =  require("body-parser"),
+      LocalStrategy         =  require("passport-local"),
+      passportLocalMongoose =  require("passport-local-mongoose"),
+      User                  =  require("./models/user");
+
+mongoose.connect("mongodb+srv://DBUser:Admin123@cluster0.z9j9r.mongodb.net/MyMindDatabase?retryWrites=true&w=majority");
+app.use(require("express-session")({
+    secret:"Any normal Word",
+    resave: false,          
+    saveUninitialized:false    
+}));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded(
+      { extended:true }
+))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req,res) =>{
+    res.render("home");
+})
+app.get("/userprofile",isLoggedIn ,(req,res) =>{
+    res.render("userprofile");
+})
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/userprofile",
+    failureRedirect:"/login"
+}),function (req, res){
+});
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+app.post("/register",(req,res)=>{
+    
+    User.register(new User({username: req.body.username,email:req.body.email,country: req.body.country}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.render("register");
+        }
+    passport.authenticate("local")(req,res,function(){
+        res.redirect("/login");
+    })    
+    })
+})
+app.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/");
+});
+function isLoggedIn(req,res,next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+app.listen(process.env.PORT ||8000,function (err) {
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Server Started At Port 8000");
+    }
+      
+});
