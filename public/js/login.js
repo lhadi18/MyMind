de7b16d24f142e95282172b05190d6b7abb7423a
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2022-05-07 00:14:00 -0700

    fixing bug from my last push

---
+++ public/js/login.js
@@ -0,0 +1,19 @@
+$('#loginBtn').click(() => {
+    $.ajax({
+        url: '/login',
+        type: 'POST',
+        data: {
+            email: $("#email").val(),
+            password: $("#password").val(),
+        }, success: function (data) {
+            console.log(data);
+            if (data == "NoEmailExist") {
+                document.getElementById("loginErrorMessage").innerHTML = "User with that email does not exist"
+            } else if (data == "wrongPassword") {
+                document.getElementById("loginErrorMessage").innerHTML = "Wrong password"
+            } else {
+                    window.location = '/userprofile'
+            }
+        }
+    })
+});
