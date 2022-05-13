a6077616db891e4e74e020c322fd4631f2b586d8
Towa Quimbayo <towacurtis@hotmail.com>
2022-05-12 19:39:17 -0700

    Started Chat Page

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,155 @@
-var _0x3aefdc=_0xd881;function _0x4b28(){var _0x5a2857=['<h4>Therapist\x20Details</h4>','<span>\x20/\x20session</span></h2>','24195OPvaTW','maps','9102570DPPPry','<p>','\x20years\x20of\x20experience</p>','<h2>$','748458hpybqN','/getTherapists','firstName','Map','</div>','ajax','2836250oIAapB','map','</p>','GET','<div\x20class=\x22therapyCard\x20mainCard\x22>','<div><a\x20href=\x22/therapists\x22>View\x20Therapist</a></div>','yearsExperience','7VizBPT','36lSCvFl','Marker','40PQcgPa','getElementById','52178FeiRjT','12735415kLnnOb','<div\x20class=\x22therapyCard\x22>','sessionCost','therapistCards','7724406cmYvDk','lastName','innerHTML','392RvkIGG'];_0x4b28=function(){return _0x5a2857;};return _0x4b28();}function _0xd881(_0x3a1f18,_0x54328f){var _0x4b28f6=_0x4b28();return _0xd881=function(_0xd8818c,_0x4176c5){_0xd8818c=_0xd8818c-0xbe;var _0x2d913e=_0x4b28f6[_0xd8818c];return _0x2d913e;},_0xd881(_0x3a1f18,_0x54328f);}(function(_0x58c22f,_0x22424e){var _0x19881d=_0xd881,_0x4f94d9=_0x58c22f();while(!![]){try{var _0x313efb=-parseInt(_0x19881d(0xd6))/0x1*(-parseInt(_0x19881d(0xda))/0x2)+-parseInt(_0x19881d(0xc2))/0x3*(parseInt(_0x19881d(0xbf))/0x4)+-parseInt(_0x19881d(0xce))/0x5+parseInt(_0x19881d(0xdf))/0x6*(-parseInt(_0x19881d(0xd5))/0x7)+parseInt(_0x19881d(0xd8))/0x8*(parseInt(_0x19881d(0xc8))/0x9)+parseInt(_0x19881d(0xc4))/0xa+parseInt(_0x19881d(0xdb))/0xb;if(_0x313efb===_0x22424e)break;else _0x4f94d9['push'](_0x4f94d9['shift']());}catch(_0x65a025){_0x4f94d9['push'](_0x4f94d9['shift']());}}}(_0x4b28,0xbdf1f),$[_0x3aefdc(0xcd)]({'url':_0x3aefdc(0xc9),'type':_0x3aefdc(0xd1),'success':function(_0x41ac06){var _0xeb8daf=_0x3aefdc,_0xa3f16d=_0x41ac06[0x1],_0x4740b3=_0x41ac06[0x2],_0x2332ae=_0x41ac06[0x0],_0x3b7ec2='<div\x20class=\x22therapyCard\x22>';_0x3b7ec2+=_0xeb8daf(0xc7)+_0xa3f16d[_0xeb8daf(0xdd)]+_0xeb8daf(0xc1),_0x3b7ec2+=_0xeb8daf(0xc0),_0x3b7ec2+='<p>'+_0xa3f16d[_0xeb8daf(0xca)]+'\x20'+_0xa3f16d['lastName']+_0xeb8daf(0xd0),_0x3b7ec2+=_0xeb8daf(0xc5)+_0xa3f16d[_0xeb8daf(0xd4)]+_0xeb8daf(0xc6),_0x3b7ec2+=_0xeb8daf(0xd3),_0x3b7ec2+=_0xeb8daf(0xcc);var _0x46396d=_0xeb8daf(0xd2);_0x46396d+='<h2>$'+_0x4740b3[_0xeb8daf(0xdd)]+'<span>\x20/\x20session</span></h2>',_0x46396d+=_0xeb8daf(0xc0),_0x46396d+=_0xeb8daf(0xc5)+_0x4740b3[_0xeb8daf(0xca)]+'\x20'+_0x4740b3[_0xeb8daf(0xe0)]+_0xeb8daf(0xd0),_0x46396d+=_0xeb8daf(0xc5)+_0x4740b3[_0xeb8daf(0xd4)]+_0xeb8daf(0xc6),_0x46396d+=_0xeb8daf(0xd3),_0x46396d+=_0xeb8daf(0xcc);var _0x3534d4=_0xeb8daf(0xdc);_0x3534d4+=_0xeb8daf(0xc7)+_0x2332ae['sessionCost']+_0xeb8daf(0xc1),_0x3534d4+=_0xeb8daf(0xc0),_0x3534d4+='<p>'+_0x2332ae[_0xeb8daf(0xca)]+'\x20'+_0x2332ae[_0xeb8daf(0xe0)]+_0xeb8daf(0xd0),_0x3534d4+=_0xeb8daf(0xc5)+_0x2332ae['yearsExperience']+_0xeb8daf(0xc6),_0x3534d4+=_0xeb8daf(0xd3),_0x3534d4+=_0xeb8daf(0xcc),document[_0xeb8daf(0xd9)](_0xeb8daf(0xde))[_0xeb8daf(0xbe)]+=_0x3b7ec2,document['getElementById']('therapistCards')['innerHTML']+=_0x46396d,document[_0xeb8daf(0xd9)](_0xeb8daf(0xde))[_0xeb8daf(0xbe)]+=_0x3534d4;}}));function initMap(){var _0x237d07=_0x3aefdc;const _0x4b720d={'lat':49.2484615,'lng':-123.0048777},_0x19a375=new google[(_0x237d07(0xc3))][(_0x237d07(0xcb))](document[_0x237d07(0xd9)](_0x237d07(0xcf)),{'zoom':0xf,'center':_0x4b720d}),_0x252737=new google[(_0x237d07(0xc3))][(_0x237d07(0xd7))]({'position':_0x4b720d,'map':_0x19a375});}window['initMap']=initMap;
+$.ajax({
+    url: '/getTherapists',
+    type: "GET",
+    success: function (data) {
+        var firstCard = data[1];
+        var mainCard = data[2];
+        var thirdCard = data[0];
+        var x = '<div class="therapyCard">';
+        x += `<h2>$${firstCard.sessionCost}<span> / session</span></h2>`
+        x += '<h4>Therapist Details</h4>'
+        x += `<p>${firstCard.firstName} ${firstCard.lastName}</p>`
+        x += `<p>${firstCard.yearsExperience} years of experience</p>`
+        x += '<div><a href="/therapists">View Therapist</a></div>'
+        x += '</div>'
+        var y = '<div class="therapyCard mainCard">';
+        y += `<h2>$${mainCard.sessionCost}<span> / session</span></h2>`
+        y += '<h4>Therapist Details</h4>'
+        y += `<p>${mainCard.firstName} ${mainCard.lastName}</p>`
+        y += `<p>${mainCard.yearsExperience} years of experience</p>`
+        y += '<div><a href="/therapists">View Therapist</a></div>'
+        y += '</div>'
+        var z = '<div class="therapyCard">';
+        z += `<h2>$${thirdCard.sessionCost}<span> / session</span></h2>`
+        z += '<h4>Therapist Details</h4>'
+        z += `<p>${thirdCard.firstName} ${thirdCard.lastName}</p>`
+        z += `<p>${thirdCard.yearsExperience} years of experience</p>`
+        z += '<div><a href="/therapists">View Therapist</a></div>'
+        z += '</div>'
+        document.getElementById("therapistCards").innerHTML += x;
+        document.getElementById("therapistCards").innerHTML += y;
+        document.getElementById("therapistCards").innerHTML += z;
+    }
+})
+
+// Google Maps
+function initMap() {
+    const uluru = { lat: 49.2484615, lng: -123.0048777 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: uluru,
+    });
+    const marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+    });
+}
+
+window.initMap = initMap;
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Chat Box
+var element = $('.therapistChat');
+var myStorage = localStorage;
+
+// if (!myStorage.getItem('chatID')) {
+//     myStorage.setItem('chatID', createUUID());
+// }
+
+element.addClass('enter');
+element.click(openElement);
+
+
+function openElement() {
+    var messages = element.find('.messages');
+    var textInput = element.find('.text-box');
+    element.find('>i').hide();
+    element.addClass('expand');
+    element.find('.chatContainer').addClass('enter');
+    var strLength = textInput.val().length * 2;
+    textInput.keydown(onMetaAndEnter).prop("disabled", false).focus();
+    element.off('click', openElement);
+    element.find('.header button').click(closeElement);
+    element.find('#sendMessage').click(sendNewMessage);
+    messages.scrollTop(messages.prop("scrollHeight"));
+    textInput.focus(function() {
+      if ($(this).text() === "Type here..."){
+          $(this).text("").focus();
+      }
+    });
+    textInput.blur(function() {
+        if ($(this).text() === ""){
+            $(this).text("Type here...");
+        }
+    });
+  textInput.blur();
+}
+
+function closeElement() {
+    element.find('.chatContainer').removeClass('enter').hide();
+    element.find('>i').show();
+    element.removeClass('expand');
+    element.find('.header button').off('click', closeElement);
+    element.find('#sendMessage').off('click', sendNewMessage);
+    element.find('.text-box').off('keydown', onMetaAndEnter).prop("disabled", true).blur();
+    setTimeout(function() {
+        element.find('.chatContainer').removeClass('enter').show()
+        element.click(openElement);
+    }, 500);
+}
+
+// function createUUID() {
+//     // http://www.ietf.org/rfc/rfc4122.txt
+//     var s = [];
+//     var hexDigits = "0123456789abcdef";
+//     for (var i = 0; i < 36; i++) {
+//         s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+//     }
+//     s[14] = "4"; // bits 12-15 of the time_hi_and_version field to 0010
+//     s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1); // bits 6-7 of the clock_seq_hi_and_reserved to 01
+//     s[8] = s[13] = s[18] = s[23] = "-";
+
+//     var uuid = s.join("");
+//     return uuid;
+// }
+
+function sendNewMessage() {
+    var userInput = $('.text-box');
+    var newMessage = userInput.html().replace(/\<div\>|\<br.*?\>/ig, '\n').replace(/\<\/div\>/g, '').trim().replace(/\n/g, '<br>');
+
+    if (!newMessage) return;
+
+    var messagesContainer = $('.messages');
+
+    messagesContainer.append([
+        '<li class="self">',
+        newMessage,
+        '</li>'
+    ].join(''));
+
+    // clean out old message
+    userInput.html('');
+    // focus on input
+    userInput.focus();
+
+    messagesContainer.finish().animate({
+        scrollTop: messagesContainer.prop("scrollHeight")
+    }, 250);
+}
+
+function onMetaAndEnter(event) {
+    if ((event.metaKey || event.ctrlKey) && event.keyCode == 13) {
+        sendNewMessage();
+    }
+}
