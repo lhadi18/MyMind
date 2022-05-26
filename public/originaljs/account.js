a4b73b7c499d614f9d57df63cf29d140c846934c
Pedro Sales Muniz <46171624+salesp07@users.noreply.github.com>
2022-05-26 00:41:43 -0700

    Finished reducing functions' sizes in account.js

--- public/originaljs/account.js
+++ public/originaljs/account.js
@@ -35,12 +35,11 @@ $.ajax({
         $("#yearsExperience").attr("value", `${data.yearsExperience}`)
         $("#sessionCost").attr("value", `${data.sessionCost}`)
         if (!data.phoneNum) {
-            $("#phone").attr("value",)
+            $("#phone").attr("value", )
             $("#phonemobile").text()
         } else {
-            let phonenumber = data.phoneNum;
-            $("#phone").attr("value", phonenumber)
-            $("#phonemobile").text(phonenumber)
+            $("#phone").attr("value", data.phoneNum)
+            $("#phonemobile").text(data.phoneNum)
         }
         if (data.userType == "therapist") {
             var displayTherapist = document.querySelectorAll(".therapistOptions");
@@ -63,112 +62,110 @@ setTimeout(() => {
     })
 }, 50);
 
+function hideErrorMessages() {
+    document.getElementById("phoneErrorMessage").style.display = 'none';
+    document.getElementById("emailErrorMessage").style.display = 'none';
+    document.getElementById("usernameErrorMessage").style.display = 'none';
+    document.getElementById("validationErrorMessage").style.display = 'none';
+}
+
+function serverInputValidation(data) {
+    let validated = false;
+    if (data == "existingEmail") {
+        hideErrorMessages();
+        document.getElementById("emailErrorMessage").style.display = 'block';
+        document.getElementById("emailErrorMessage").innerHTML = "A user with that email already exists";
+    } else if (data == "existingPhone") {
+        hideErrorMessages();
+        document.getElementById("phoneErrorMessage").style.display = 'block';
+        document.getElementById("phoneErrorMessage").innerHTML = "A user with that phone number already exists";
+    } else if (data == "existingUsername") {
+        hideErrorMessages();
+        document.getElementById("usernameErrorMessage").style.display = 'block';
+        document.getElementById("usernameErrorMessage").innerHTML = "A user with that username already exists";
+    } else if (data == "passwordValidation") {
+        hideErrorMessages();
+        document.getElementById("validationErrorMessage").style.display = 'block';
+        document.getElementById("validationErrorMessage").innerHTML = "Password must be at least 5 or less than 20 characters long";
+    } else {
+        validated = true
+    }
+    return validated;
+}
+
+function handleEditSuccess(data) {
+    if (data == "updated") {
+        if (document.getElementById("password").value == "") {
+            hideErrorMessages();
+            document.getElementById('profileSuccessModal').style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+            setTimeout(() => {
+                window.location = '/userprofile'
+            }, 2500);
+        } else {
+            $.post("/logout");
+            hideErrorMessages()
+            document.getElementById('profileSuccessModal').style.display = 'flex';
+            document.body.style.overflow = 'hidden';
+            setTimeout(() => {
+                window.location = '/login'
+            }, 2500);
+        }
+    } else if (data == "logout") {
+        window.location = '/login'
+    }
+}
+
+function editProfile() {
+    $.ajax({
+        url: '/editProfile',
+        type: 'POST',
+        data: {
+            firstname: $("#firstname").val().charAt(0).toUpperCase() + $("#firstname").val().substring(1),
+            lastname: $("#lastname").val().charAt(0).toUpperCase() + $("#lastname").val().substring(1),
+            email: $("#email").val().toLowerCase(),
+            username: $("#username").val().toLowerCase(),
+            phone: $("#phone").val(),
+            password: $("#password").val(),
+            yearsExperience: $("#yearsExperience").val(),
+            sessionCost: $("#sessionCost").val(),
+        },
+        success: function (data) {
+            if (serverInputValidation(data)) {
+                handleEditSuccess(data);
+            }
+        }
+    })
+}
+
 $('#saveChanges').click(() => {
     var emp = document.getElementById("password").value;
     var phoneLength = $("#phone").val();
     if (phoneLength.length != 10) {
+        hideErrorMessages();
         document.getElementById("phoneErrorMessage").style.display = 'block';
-        document.getElementById("emailErrorMessage").style.display = 'none';
-        document.getElementById("usernameErrorMessage").style.display = 'none';
-        document.getElementById("validationErrorMessage").style.display = 'none';
         document.getElementById("phoneErrorMessage").innerHTML = "Your phone number must be of length 10";
     } else if (!isEmail($("#email").val())) {
-        document.getElementById("phoneErrorMessage").style.display = 'none';
+        hideErrorMessages();
         document.getElementById("emailErrorMessage").style.display = 'block';
-        document.getElementById("usernameErrorMessage").style.display = 'none';
-        document.getElementById("validationErrorMessage").style.display = 'none';
         document.getElementById("emailErrorMessage").innerHTML = "Please follow this email pattern: example@email.com";
     } else if (inputValidation()) {
         window.scrollTo(0, document.body.scrollHeight);
-        document.getElementById("phoneErrorMessage").style.display = 'none';
-        document.getElementById("emailErrorMessage").style.display = 'none';
-        document.getElementById("usernameErrorMessage").style.display = 'none';
+        hideErrorMessages();
         document.getElementById("validationErrorMessage").style.display = 'block';
-        window.scrollTo(0, document.body.scrollHeight);
         document.getElementById("validationErrorMessage").innerHTML = "There are empty fields";
     } else if (negativeValidation()) {
         window.scrollTo(0, document.body.scrollHeight);
-        document.getElementById("phoneErrorMessage").style.display = 'none';
-        document.getElementById("emailErrorMessage").style.display = 'none';
-        document.getElementById("usernameErrorMessage").style.display = 'none';
+        hideErrorMessages();
         document.getElementById("validationErrorMessage").style.display = 'block';
         document.getElementById("validationErrorMessage").innerHTML = "Experience or cost of session cannot be less than 0";
+    } else if ($("#password").val() != "" && passwordValidation()) {
+        window.scrollTo(0, document.body.scrollHeight);
+        hideErrorMessages();
+        document.getElementById("validationErrorMessage").style.display = 'block';
+        document.getElementById("validationErrorMessage").innerHTML = "Password must be at least 5 or less than 20 characters long";
     } else {
-        if ($("#password").val() != "" && passwordValidation()) {
-            window.scrollTo(0, document.body.scrollHeight);
-            document.getElementById("phoneErrorMessage").style.display = 'none';
-            document.getElementById("emailErrorMessage").style.display = 'none';
-            document.getElementById("usernameErrorMessage").style.display = 'none';
-            document.getElementById("validationErrorMessage").style.display = 'block';
-            document.getElementById("validationErrorMessage").innerHTML = "Password must be at least 5 or less than 20 characters long";
-        } else {
-            $.ajax({
-                url: '/editProfile',
-                type: 'POST',
-                data: {
-                    firstname: $("#firstname").val().charAt(0).toUpperCase() + $("#firstname").val().substring(1),
-                    lastname: $("#lastname").val().charAt(0).toUpperCase() + $("#lastname").val().substring(1),
-                    email: $("#email").val().toLowerCase(),
-                    username: $("#username").val().toLowerCase(),
-                    phone: $("#phone").val(),
-                    password: $("#password").val(),
-                    yearsExperience: $("#yearsExperience").val(),
-                    sessionCost: $("#sessionCost").val(),
-                }, success: function (data) {
-                    console.log(data);
-                    if (data == "existingEmail") {
-                        document.getElementById("phoneErrorMessage").style.display = 'none';
-                        document.getElementById("emailErrorMessage").style.display = 'block';
-                        document.getElementById("usernameErrorMessage").style.display = 'none';
-                        document.getElementById("validationErrorMessage").style.display = 'none';
-                        document.getElementById("emailErrorMessage").innerHTML = "A user with that email already exists";
-                    } else if (data == "existingPhone") {
-                        document.getElementById("phoneErrorMessage").style.display = 'block';
-                        document.getElementById("emailErrorMessage").style.display = 'none';
-                        document.getElementById("usernameErrorMessage").style.display = 'none';
-                        document.getElementById("validationErrorMessage").style.display = 'none';
-                        document.getElementById("phoneErrorMessage").innerHTML = "A user with that phone number already exists";
-                    } else if (data == "existingUsername") {
-                        document.getElementById("phoneErrorMessage").style.display = 'none';
-                        document.getElementById("emailErrorMessage").style.display = 'none';
-                        document.getElementById("usernameErrorMessage").style.display = 'block';
-                        document.getElementById("validationErrorMessage").style.display = 'none';
-                        document.getElementById("usernameErrorMessage").innerHTML = "A user with that username already exists";
-                    } else if (data == "passwordValidation") {
-                        document.getElementById("phoneErrorMessage").style.display = 'none';
-                        document.getElementById("emailErrorMessage").style.display = 'none';
-                        document.getElementById("usernameErrorMessage").style.display = 'none';
-                        document.getElementById("validationErrorMessage").style.display = 'block';
-                        document.getElementById("validationErrorMessage").innerHTML = "Password must be at least 5 or less than 20 characters long";
-                    } else if (data == "updated") {
-                        if (emp == "") {
-                            document.getElementById("phoneErrorMessage").style.display = 'none';
-                            document.getElementById("emailErrorMessage").style.display = 'none';
-                            document.getElementById("usernameErrorMessage").style.display = 'none';
-                            document.getElementById("validationErrorMessage").style.display = 'none';
-                            document.getElementById('profileSuccessModal').style.display = 'flex';
-                            document.body.style.overflow = 'hidden';
-                            setTimeout(() => {
-                                window.location = '/userprofile'
-                            }, 2500);
-                        } else {
-                            $.post("/logout");
-                            document.getElementById("phoneErrorMessage").style.display = 'none';
-                            document.getElementById("emailErrorMessage").style.display = 'none';
-                            document.getElementById("usernameErrorMessage").style.display = 'none';
-                            document.getElementById("validationErrorMessage").style.display = 'none';
-                            document.getElementById('profileSuccessModal').style.display = 'flex';
-                            document.body.style.overflow = 'hidden';
-                            setTimeout(() => {
-                                window.location = '/login'
-                            }, 2500);
-                        }
-                    } else if (data == "logout") {
-                        window.location = '/login'
-                    }
-                }
-            })
-        }
+        editProfile();
     }
 });
 
@@ -178,15 +175,14 @@ function isEmail(email) {
 }
 
 function inputValidation() {
-    console.log(currentType)
     const inpObjFirstName = document.getElementById("firstname");
     const inpObjLastName = document.getElementById("lastname");
     const inpObjUsername = document.getElementById("username");
     const inpObjExperience = document.getElementById("yearsExperience");
     const inpObjSession = document.getElementById("sessionCost");
     if (currentType == "therapist") {
-        if (!inpObjFirstName.checkValidity() || !inpObjLastName.checkValidity() || !inpObjUsername.checkValidity()
-            || !inpObjExperience.checkValidity() || !inpObjSession.checkValidity()) {
+        if (!inpObjFirstName.checkValidity() || !inpObjLastName.checkValidity() || !inpObjUsername.checkValidity() ||
+            !inpObjExperience.checkValidity() || !inpObjSession.checkValidity()) {
             return true;
         }
     } else {
@@ -237,7 +233,6 @@ if (window.location.pathname == '/userprofile') {
                 url: '/deleteUserProfile',
                 type: 'DELETE',
                 success: function (data) {
-                    console.log(data);
                     if (data == 'lastAdmin') {
                         document.getElementById("deleteAccountErrorMessage").style.display = 'block';
                         $('#deleteAccountErrorMessage').html('Deletion failed. Database needs to have at least 1 administrator.')
@@ -253,6 +248,7 @@ if (window.location.pathname == '/userprofile') {
             })
         }
     }
+
     document.getElementById('mobDeleteAccount').onclick = function (e) {
         deleteUserModal.style.display = "block";
         document.body.style.overflow = 'hidden';
@@ -271,4 +267,4 @@ if (window.location.pathname == '/userprofile') {
             document.body.style.overflow = 'auto';
         }
     }
-} 
+}
